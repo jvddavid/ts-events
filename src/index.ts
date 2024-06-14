0e78468e184758dedc184c206a93eff340786aa0
João Victor <j.victordavid2@gmail.com>
2024-06-14 00:18:20 -0300

    feat: add Type for events library

---
+++ src/index.ts
@@ -0,0 +1,67 @@
+import { EventEmitter as VanillaEventEmitter } from 'events'
+
+export type EventMap = Record<string, any[]>
+export type EventMapKeys<T extends EventMap> = keyof T & string
+export type EventMapArgs<T extends EventMap, K extends EventMapKeys<T>> = T[K]
+
+export class EventEmitter<ListenEvents extends EventMap = EventMap, EmitEvents extends EventMap = ListenEvents> extends VanillaEventEmitter {
+  emit<EventName extends EventMapKeys<EmitEvents>, EventArgs extends EventMapArgs<EmitEvents, EventName>>(eventName: EventName, ...eventArg: EventArgs) {
+    return super.emit(eventName, ...eventArg)
+  }
+
+  addListener<EventName extends EventMapKeys<ListenEvents>, EventArgs extends EventMapArgs<ListenEvents, EventName>>(
+    eventName: EventName,
+    handler: (...eventArg: EventArgs) => void
+  ) {
+    return super.addListener(eventName, handler)
+  }
+
+  on<EventName extends EventMapKeys<ListenEvents>, EventArgs extends EventMapArgs<ListenEvents, EventName>>(eventName: EventName, handler: (...eventArg: EventArgs) => void) {
+    return super.on(eventName, handler)
+  }
+
+  once<EventName extends EventMapKeys<ListenEvents>, EventArgs extends EventMapArgs<ListenEvents, EventName>>(eventName: EventName, handler: (...eventArg: EventArgs) => void) {
+    return super.once(eventName, handler)
+  }
+
+  prependListener<EventName extends EventMapKeys<ListenEvents>, EventArgs extends EventMapArgs<ListenEvents, EventName>>(
+    eventName: EventName,
+    handler: (...eventArg: EventArgs) => void
+  ) {
+    return super.prependListener(eventName, handler)
+  }
+
+  prependOnceListener<EventName extends EventMapKeys<ListenEvents>, EventArgs extends EventMapArgs<ListenEvents, EventName>>(
+    eventName: EventName,
+    handler: (...eventArg: EventArgs) => void
+  ) {
+    return super.prependOnceListener(eventName, handler)
+  }
+
+  removeListener<EventName extends EventMapKeys<ListenEvents>, EventArgs extends EventMapArgs<ListenEvents, EventName>>(
+    eventName: EventName,
+    handler: (...eventArg: EventArgs) => void
+  ) {
+    return super.removeListener(eventName, handler)
+  }
+
+  off<EventName extends EventMapKeys<ListenEvents>, EventArgs extends EventMapArgs<ListenEvents, EventName>>(eventName: EventName, handler: (...eventArg: EventArgs) => void) {
+    return super.off(eventName, handler)
+  }
+
+  removeAllListeners<EventName extends EventMapKeys<ListenEvents>>(eventName?: EventName) {
+    return super.removeAllListeners(eventName)
+  }
+
+  listeners<EventName extends EventMapKeys<ListenEvents>, EventArgs extends EventMapArgs<ListenEvents, EventName>>(eventName: EventName) {
+    return super.listeners(eventName) as ((...eventArg: EventArgs) => void)[]
+  }
+
+  listenerCount<EventName extends EventMapKeys<ListenEvents>>(eventName: EventName) {
+    return super.listenerCount(eventName)
+  }
+
+  rawListeners<EventName extends EventMapKeys<ListenEvents>, EventArgs extends EventMapArgs<ListenEvents, EventName>>(eventName: EventName) {
+    return super.rawListeners(eventName) as ((...eventArg: EventArgs) => void)[]
+  }
+}
